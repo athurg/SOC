e36239f22b1aca0ffcccd7fd0eb54495f9084658
Athurg Gooth <athurg@gooth.org>
2013-07-21 10:52:13 +0800

    Add demo MCU code of 8051 core

---
+++ 8051/MAIN.C
@@ -0,0 +1,19 @@
+#include "common.h"
+#include "interrupt.h"
+
+uchar counter=0;
+void main()
+{
+	interrupt_switch(IE0_volt_VECTOR,INIT_ENABLE);
+	interrupt_switch(INIT_SWITCH,INIT_ENABLE);
+	while(1){
+		P1=counter;
+	}
+}
+
+void INIT0_ISR(void) __interrupt IE0_VECTOR
+{
+    //外部中断0中断处理程序
+	counter++;
+}
+
